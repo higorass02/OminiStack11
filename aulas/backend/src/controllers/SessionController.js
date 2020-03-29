23e16115c2347176812ed28c40821d205583609c
Higor Santos <higoraugusto120@gmail.com>
2020-03-29 16:15:59 -0300

    subindo 2 dia completo

---
+++ aulas/backend/src/controllers/SessionController.js
@@ -0,0 +1,17 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async index (request,response){    
+        const { id } = request.body;
+
+        const ongs = await connection('ongs')
+        .where('id',id)
+        .select('name')
+        .first();
+
+        if(!ongs){
+            return response.status(401).json({ error: 'No Ong found with this ID' });
+        }
+        return response.json(ongs);
+    },
+}
