0186c34047eabfc7d20c2c18c7e42bd9aefed5ad
Higor Santos <higoraugusto120@gmail.com>
2020-03-31 22:35:49 -0300

    zerando codigos

--- aulas/backend/src/controllers/ProfileController.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../database/connection');
-
-module.exports = {
-    async index (request,response){    
-        const omg_id = request.headers.authorization;
-        const incidents = await connection('incidents').select('*').where('omg_id', omg_id).first();
-        return response.json(incidents);
-    },
-}
